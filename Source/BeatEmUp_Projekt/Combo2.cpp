f9a93d8313c0514ba0d4f613f26b2f55c24d7bb4
Sushiy <julivizi@yahoo.de>
2015-07-11 18:24:52 +0200

    Changed to Combostruct, implemented bruteforce inputmatching

---
+++ Source/BeatEmUp_Projekt/Combo2.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BeatEmUp_Projekt.h"
+#include "Combo2.h"
+
+Combo2::Combo2()
+{
+}
+
+Combo2::~Combo2()
+{
+}
